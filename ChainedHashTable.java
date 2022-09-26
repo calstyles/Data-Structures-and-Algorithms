a07300c0b3b9839b7187842a5ee20ceb7e7232e8
calstyles <78227433+calstyles@users.noreply.github.com>
2022-09-26 12:16:36 -0400

    Add files via upload

--- ChainedHashTable.java
+++ ChainedHashTable.java
@@ -2,14 +2,13 @@ import java.util.LinkedList;
 
 public class ChainedHashTable {
 
-	public LinkedList<Integer>[] linkedArray;
+	private LinkedList<Integer>[] linkedArray;
 
 	
 	public ChainedHashTable(int n) {
 		int helper = (int)(Math.log(n) / Math.log(2));
 		int m = (int) Math.pow(2, helper + 1);
-		linkedArray = new LinkedList<Integer>[m];
-		
+		linkedArray = new LinkedList[m];		
 	}
 	
 	public insert() {
